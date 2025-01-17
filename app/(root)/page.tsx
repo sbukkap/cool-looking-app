c09954b43cc942817b74f36c9aed6a89087f8823
nothingbutCode4now <shreekar.shredder2002@gmail.com>
2025-01-17 14:55:40 -0500

    created queries.ts to send data fetch queries to sanity, implemented automatic sanity typegen for groq queries, used sanityLive to fetch render live data(here, a startup) the moment u publish it in sanity studio(really cool btw)

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,6 @@
-import StartupCard from "@/components/StartupCard";
-// import { STARTUPS_QUERY } from "@/sanity/lib/queries";
-// import { sanityFetch, SanityLive } from "@/sanity/lib/live";
+import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
+import { STARTUPS_QUERY } from "@/sanity/lib/queries";
+import { sanityFetch, SanityLive } from "@/sanity/lib/live";
 import { auth } from "@/auth";
 import SearchForm from "@/components/SearchForm";
 
@@ -16,22 +16,7 @@ export default async function Home({
 
   // console.log(session?.id);
 
-  // const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY, params });
-
-  const posts = [
-    {
-      _createdAt: "Yesterday",
-      views: 55,
-      author: { _id: 1 },
-      _id: 1,
-      description: "This is a description",
-      image:
-        "https://images.unsplash.com/photo-1634912314704-c646c586b131?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8FA%3D%3D",
-      category: "Robots",
-      title: "We Robots",
-    },
-  ];
-  
+  const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY, params });
 
   return (
     <>
@@ -56,7 +41,7 @@ export default async function Home({
 
         <ul className="mt-7 card_grid">
           {posts?.length > 0 ? (
-            posts.map((post) => (
+            posts.map((post: StartupTypeCard) => (
               <StartupCard key={post?._id} post={post} />
             ))
           ) : (
@@ -65,7 +50,7 @@ export default async function Home({
         </ul>
       </section>
 
-      {/* <SanityLive /> */}
+      <SanityLive />
     </>
   );
 }
