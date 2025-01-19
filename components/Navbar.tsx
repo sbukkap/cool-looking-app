8c2568f58d0b8a70b1e4daf765b74c309e8a1830
nothingbutCode4now <shreekar.shredder2002@gmail.com>
2025-01-19 13:28:46 -0500

    1.Fixed navbar to better display in small screens 2.Added 2queries to fetch author from id when user clicks on profile page, and to fetch the author startups if any 3.Added new route to user page, and 4.UserStartups component to display the users startups

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,8 @@
 import Link from "next/link";
 import Image from "next/image";
 import { auth, signOut, signIn } from "@/auth";
-// import { BadgePlus, LogOut } from "lucide-react";
-// import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { BadgePlus, LogOut } from "lucide-react";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 const Navbar = async () => {
   const session = await auth();
@@ -19,7 +19,7 @@ const Navbar = async () => {
             <>
               <Link href="/startup/create">
                 <span className="max-sm:hidden">Create</span>
-                {/* <BadgePlus className="size-6 sm:hidden" /> */}
+                <BadgePlus className="size-6 sm:hidden" />
               </Link>
 
               <form
@@ -31,11 +31,11 @@ const Navbar = async () => {
               >
                 <button type="submit">
                   <span className="max-sm:hidden">Logout</span>
-                  {/* <LogOut className="size-6 sm:hidden text-red-500" /> */}
+                  <LogOut className="size-6 sm:hidden text-red-500" />
                 </button>
               </form>
 
-              {/* <Link href={`/user/${session?.id}`}>
+              <Link href={`/user/${session?.id}`}>
                 <Avatar className="size-10">
                   <AvatarImage
                     src={session?.user?.image || ""}
@@ -43,7 +43,7 @@ const Navbar = async () => {
                   />
                   <AvatarFallback>AV</AvatarFallback>
                 </Avatar>
-              </Link> */}
+              </Link>
             </>
           ) : (
             <form
--- components/StartupCard.tsx
+++ components/StartupCard.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import { Author, Startup } from "@/sanity/types";
-// import { Skeleton } from "@/components/ui/skeleton";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export type StartupTypeCard = Omit<Startup, "author"> & { author?: Author };
 
@@ -41,8 +41,8 @@ const StartupCard = ({ post }: { post: StartupTypeCard }) => {
         </div>
         <Link href={`/user/${author?._id}`}>
           <Image
-            src="https://placehold.co/48x48"
-            alt='placeholder'
+            src={author?.image!}
+            alt={author?.image!}
             width={48}
             height={48}
             className="rounded-full"
@@ -68,14 +68,14 @@ const StartupCard = ({ post }: { post: StartupTypeCard }) => {
   );
 };
 
-// export const StartupCardSkeleton = () => (
-//   <>
-//     {[0, 1, 2, 3, 4].map((index: number) => (
-//       <li key={cn("skeleton", index)}>
-//         <Skeleton className="startup-card_skeleton" />
-//       </li>
-//     ))}
-//   </>
-// );
+export const StartupCardSkeleton = () => (
+  <>
+    {[0, 1, 2, 3, 4].map((index: number) => (
+      <li key={cn("skeleton", index)}>
+        <Skeleton className="startup-card_skeleton" />
+      </li>
+    ))}
+  </>
+);
 
 export default StartupCard;
