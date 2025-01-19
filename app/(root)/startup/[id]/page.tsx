946eecf3bf8e07765b06bdc5b1e44a1b2efd2b36
nothingbutCode4now <shreekar.shredder2002@gmail.com>
2025-01-19 15:20:15 -0500

    1.modified toast shadcn component to better display success message upon pitch creation 2.Application dev complete!

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -1,9 +1,8 @@
 import { Suspense } from "react";
 import { client } from "@/sanity/lib/client";
-// import {
-//   PLAYLIST_BY_SLUG_QUERY,
-//   STARTUP_BY_ID_QUERY,
-// } from "@/sanity/lib/queries";
+import {
+  STARTUP_BY_ID_QUERY,
+} from "@/sanity/lib/queries";
 import { notFound } from "next/navigation";
 import { formatDate } from "@/lib/utils";
 import Link from "next/link";
@@ -21,12 +20,7 @@ export const experimental_ppr = true;
 const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
   const id = (await params).id;
 
-  const [post, { select: editorPosts }] = await Promise.all([
-    client.fetch(STARTUP_BY_ID_QUERY, { id }),
-    client.fetch(PLAYLIST_BY_SLUG_QUERY, {
-      slug: "editor-picks-new",
-    }),
-  ]);
+  const post = await client.fetch(STARTUP_BY_ID_QUERY, { id });
 
   if (!post) return notFound();
 
@@ -85,18 +79,6 @@ const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
         </div>
 
         <hr className="divider" />
-
-        {/* {editorPosts?.length > 0 && (
-          <div className="max-w-4xl mx-auto">
-            <p className="text-30-semibold">Editor Picks</p>
-
-            <ul className="mt-7 card_grid-sm">
-              {editorPosts.map((post: StartupTypeCard, i: number) => (
-                <StartupCard key={i} post={post} />
-              ))}
-            </ul>
-          </div>
-        )} */}
 
         <Suspense fallback={<Skeleton className="view_skeleton" />}>
           <View id={id} />
